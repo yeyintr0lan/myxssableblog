0825ea71a98f96335ee431ef3bd664129192859c
yeyintr0lan <90928291+yeyintr0lan@users.noreply.github.com>
2021-09-25 17:05:16 +0800

    test

--- seleniumtest.py
+++ seleniumtest.py
@@ -1,12 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 import time
 import os
 
-driver = webdriver.Chrome()
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(options=chrome_options)
 #driver.maximize_window()
 driver.get('http://159.89.6.247:5000/login')
-driver.find_element_by_name("password").send_keys(os.environ['LOGINPASS'])
+#driver.find_element_by_name("password").send_keys(os.environ['LOGINPASS'])
 driver.find_element_by_name("password").send_keys("12345")
 driver.find_element_by_xpath("//button[@class='btn btn-primary']").click()
 #driver.find_element_by_class_name("mt-5") == "Hi, John!"
