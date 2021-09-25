16f18e2c6e29c489d400eb6097aa8c59f3f9e9d1
yeyintr0lan <90928291+yeyintr0lan@users.noreply.github.com>
2021-09-25 17:06:30 +0800

    using env

--- seleniumtest.py
+++ seleniumtest.py
@@ -11,8 +11,8 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(options=chrome_options)
 #driver.maximize_window()
 driver.get('http://159.89.6.247:5000/login')
-#driver.find_element_by_name("password").send_keys(os.environ['LOGINPASS'])
-driver.find_element_by_name("password").send_keys("12345")
+driver.find_element_by_name("password").send_keys(os.environ['LOGINPASS'])
+#driver.find_element_by_name("password").send_keys("12345")
 driver.find_element_by_xpath("//button[@class='btn btn-primary']").click()
 #driver.find_element_by_class_name("mt-5") == "Hi, John!"
 if(driver.find_element_by_class_name("mt-5").is_displayed()):
